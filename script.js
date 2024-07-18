af20cdb6f642252992e68bcdb01a52b088318010
E <sh0oki@users.noreply.github.com>
2024-07-18 19:02:13 +0300

    More CORS 4

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function handleSubmit(event) {
   fetch('https://gw-lmz.lamarzocco.io/v1/home/machines/' + username + '/status', {
     method: 'POST',
     headers: {
-      'accept': '*/*',
+      // 'accept': '*/*',
       'currentappos': 'ios',
       'content-type': 'application/json',
-      'accept-language': 'en-IL;q=1, he-IL;q=0.9',
+      // 'accept-language': 'en-IL;q=1, he-IL;q=0.9',
       'authorization': 'Bearer ' + password,
       'currentappversion': '3.1.4',
       'user-agent': 'La Marzocco/3.1.4 (iPhone; iOS 17.5.1; Scale/3.00)'
